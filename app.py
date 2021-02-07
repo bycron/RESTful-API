f407f813b9f42e3536bd22c86c7d7e00ab8c5418
Chase <bycron@pm.me>
2021-02-06 23:04:57 -0500

    v0.01 - api beginnings

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
 stores = [{'name': 'My Wonderful Store','items': [{'name': 'My Item','price': 15.99}]}]
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/store', methods=['POST'])
 def create_store():
@@ -30,10 +34,22 @@ def get_stores():
 
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-    pass
+    request_data = request.get_json()
+    for store in stores:
+        if name == store['name']:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(new_item)
+    return jsonify({'message': 'store not found'})
 
 @app.route('/store/<string:name>/item')
 def get_items_in_store(name):
-    pass
+    for store in stores:
+        if name == store['name']:
+            return jsonify({'items': store['items']})
+    return jsonify({'message': 'store not found'})
 
 app.run(port=5000)
